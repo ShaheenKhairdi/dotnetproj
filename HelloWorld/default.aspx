e990bf525038d368613887db5f389544c69f2181
Redmine Admin <admin@example.net>
2021-01-03 13:17:39 +0530

    Initial Commit

---
+++ HelloWorld/default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="HelloWorld._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="text-align: center;">
+    <h2>Hello World from Dot.Net</h2>
+
+        <asp:Label ID="lbl_IPAddress" runat="server" Font-Bold="True" ForeColor="Red" Font-Size="Larger"></asp:Label>
+        <p />
+        <asp:Label ID="lbl_Date" runat="server" Font-Bold="True" ForeColor="#0066FF" Font-Size="Larger"></asp:Label>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ HelloWorld/default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace HelloWorld
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //This returns the first IP4 address or null
+            IPAddress ipAddress = Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+            string strIPAddress = ipAddress.ToString();
+            lbl_IPAddress.Text = "IP Address of Server : " + strIPAddress;
+
+            lbl_Date.Text = "Current Time : " + DateTime.Now.ToString();
+        }
+    }
+}
